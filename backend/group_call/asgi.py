c802a748c9e9fa1c396d09054d753e06c02bcfc2
Bayajit Mohammed <bayajitmohammed@gmail.com>
2024-05-12 09:40:35 +0600

    package update

--- backend/group_call/asgi.py
+++ backend/group_call/asgi.py
@@ -1,21 +1,20 @@
 # group_call/asgi.py
 import os
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "group_call.settings")
 django_asgi_app = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-import video_signalling.routing
+from video_signalling.routing import websocket_urlpatterns
 
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(video_signalling.routing.websocket_urlpatterns)
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    ),
+})
+
--- backend/group_call/settings.py
+++ backend/group_call/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     env_path = Path('.') / 'group_call/.env'
@@ -42,6 +42,7 @@ else:
 
 # Application definition
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
